313555bc5d66883f3c3127ec767eb4fa9bb143c0
Tateishi Tadatoshi <ishio39@gmail.com>
2024-10-02 19:51:39 +0900

    add my-functions

--- lisp/my-functions.el
+++ lisp/my-functions.el
@@ -1,5 +1,35 @@
-;;; -*- mode: emacs-lisp; coding: utf-8 -*-
-;;; my-functions.el
+;;; my-functions.el  -*- lexical-binding: t; -*-
+
+
+;; Copyright (C) 2021 TATEISHI Tadatoshi
+
+;; Author: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Maintainer: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Created: 2021/04/09
+;; Version: 0.0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
 
 (defun my/set-show-trailing-whitespace ()
   (setq show-trailing-whitespace t))
