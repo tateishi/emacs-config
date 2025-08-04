6054914c637b77fc7ef1795e1adc28b4bf7b15e7
Tateishi Tadatoshi <ishio39@gmail.com>
2025-08-04 10:39:24 +0900

    add comment.

--- init.el
+++ init.el
@@ -1,5 +1,33 @@
-;;;; init.el --- -*- lexical-binding: t -*-
+;;;; init.el --- My init entrypoint -*- lexical-binding: t -*-
 
+
+;; Copyright (C) 2021-2025  TATEISHI Tadatoshi
+
+;; Author: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Created: 2021/04/12
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My init entrypoint.
+
+;;; Code:
 
 (setq gc-cons-threshold (* 1024 1024 1024))
 (defvar warning-minimum-level :emergency)
@@ -33,3 +61,5 @@
   :functions init-locader-load
   :init (setq init-loader-byte-compile t)
   :config (init-loader-load))
+
+;;; init.el ends here
