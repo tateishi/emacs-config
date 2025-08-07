6757a47d6efcb436b19815ebc6d991c51dee0834
Tateishi Tadatoshi <ishio39@gmail.com>
2025-08-07 14:32:49 +0900

    20-fonts.elのコメントを修正

--- inits/20-fonts.el
+++ inits/20-fonts.el
@@ -1,4 +1,33 @@
-;;;; -*- mode: emacs-lisp; coding: utf-8 -*-
+;;; 20-fonts.el --- My init for font setting  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 TATEISHI Tadatoshi
+
+;; Author: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Created: 2021/04/12
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My init for font setting.
+
+;;; Code:
+
 
 (defun my/create-fontset (font name)
   (message "%s:%s" font name)
@@ -41,4 +70,6 @@
   :init
   (my/select-favorite-font font-list))
 
-;;;; 20-fonts.el ends here
+(provide '20-fonts)
+
+;;; 20-fonts.el ends here
