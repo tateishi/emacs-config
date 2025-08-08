fc30c8e29cc5c79a8bada4bed460975fbe08f5a1
Tateishi Tadatoshi <ishio39@gmail.com>
2025-08-08 15:07:36 +0900

    設定の条件実行をwhenに変更

--- inits/50-core.el
+++ inits/50-core.el
@@ -525,26 +525,25 @@
 ;;;
 ;;; migemo
 ;;;
-
-(use-package migemo
-  :if (executable-find "cmigemo")
-  :ensure t
-  :preface
-  (setq my-migemo-path "/usr/share/cmigemo/utf-8")
-  (defun my-migemo-directory (path)
-    (if (file-directory-p path)
-        path
-      (let ((migemo-path (file-name-directory (or (executable-find "cmigemo") ""))))
-        (expand-file-name "dict/utf8" migemo-path))))
-  (defun my-migemo-dictionary (path)
-    (if (file-directory-p path)
-        (expand-file-name "migemo-dict" path)))
-  :custom
-  (migemo-directory (my-migemo-directory my-migemo-path))
-  (migemo-dictionary (my-migemo-dictionary my-migemo-path))
-  :functions migemo-init
-  :config
-  (migemo-init))
+(when (executable-find "cmigemo")
+  (use-package migemo
+    :ensure t
+    :preface
+    (setq my-migemo-path "/usr/share/cmigemo/utf-8")
+    (defun my-migemo-directory (path)
+      (if (file-directory-p path)
+          path
+        (let ((migemo-path (file-name-directory (or (executable-find "cmigemo") ""))))
+          (expand-file-name "dict/utf8" migemo-path))))
+    (defun my-migemo-dictionary (path)
+      (if (file-directory-p path)
+          (expand-file-name "migemo-dict" path)))
+    :custom
+    (migemo-directory (my-migemo-directory my-migemo-path))
+    (migemo-dictionary (my-migemo-dictionary my-migemo-path))
+    :functions migemo-init
+    :config
+    (migemo-init)))
 
 ;;;
 ;;; open junk file
