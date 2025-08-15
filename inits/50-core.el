4bd6ee1232fd0a466d31ebe4503af6f36761667d
Tateishi Tadatoshi <ishio39@gmail.com>
2025-08-16 00:05:11 +0900

    orgのタイムスタンプ整理

--- inits/50-core.el
+++ inits/50-core.el
@@ -112,8 +112,7 @@
   :custom
   (org-agenda-files (if (file-exists-p org-directory)
                         (directory-files-recursively org-directory "\\.org$")))
-  (org-time-stamp-custom-formats '("<%Y-%m-%d %a>" . "<%Y-%m-%d %a %H:%M>"))
-  (org-display-custom-times t))
+  (org-timestamp-formats '("%Y-%m-%d %a" . "%Y-%m-%d %a %H:%M")))
 
 (use-package org-journal
   :ensure t
