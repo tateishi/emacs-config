8d6a18d1a56f19476519df88c13a7af9462dfb7f
TATEISHI Tadatoshi <ishio39@gmail.com>
2026-03-06 11:12:55 +0900

    引用符の付け方を変更

--- scripts/install-packages.ps1
+++ scripts/install-packages.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
 $Emacs   = $env:EMACS
-if (-not $Emacs) { $Emacs = "emacs.exe" }   # 必要に応じてフルパス
+if (-not $Emacs) { $Emacs = "$env:LOCALAPPDATA\Emacs\bin\emacs.exe" }   # 必要に応じてフルパス
 $PkgList = if ($args.Length -gt 0) { $args[0] } else { "packages.txt" }
 
 & $Emacs -Q --batch -l scripts/batch-install.el `
-   --eval "(batch-install-packages `"$PkgList`")"
+   --eval "(batch-install-packages \`"$PkgList\`")"
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
